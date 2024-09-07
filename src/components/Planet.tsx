4d1846ddd4278429044c554e7b449f01c3a180ec
Himanshu Jain <jainh246@gmail.com>
2024-09-08 01:12:06 +0530

    refactored planet

--- src/components/Planet.tsx
+++ src/components/Planet.tsx
@@ -10,26 +10,28 @@ interface Props {
   distance: number;
 }
 
-export const Planet = ({ radius, color, distance }: Props) => {
+export const Planet = ({
+  radius: planetRadius,
+  color: planetColor,
+  distance: orbitDistance,
+}: Props) => {
   const planetRef = useRef<Mesh>(null);
 
   useFrame((state, delta) => {
-    // Update planet rotation and position here
-    // For example:
-    const rotationSpeed = 1;
-    const orbitSpeed = 0.05;
     if (planetRef.current) {
+      const rotationSpeed = 1;
+      const orbitSpeed = 0.05;
       planetRef.current.rotation.y += rotationSpeed * delta;
       planetRef.current.position.x =
-        distance * Math.sin(orbitSpeed * state.clock.elapsedTime);
+        orbitDistance * Math.sin(orbitSpeed * state.clock.elapsedTime);
       planetRef.current.position.z =
-        distance * Math.cos(orbitSpeed * state.clock.elapsedTime);
+        orbitDistance * Math.cos(orbitSpeed * state.clock.elapsedTime);
     }
   });
 
   return (
-    <Sphere ref={planetRef} args={[radius, 32, 32]}>
-      <meshBasicMaterial color={color} />
+    <Sphere ref={planetRef} args={[planetRadius, 32, 32]}>
+      <meshBasicMaterial color={planetColor} />
     </Sphere>
   );
 };
